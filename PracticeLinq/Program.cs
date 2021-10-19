4335d1f559844892e36de406b4eb6c574510fd49
RUDepure <alexrum92@gmail.com>
2021-10-18 22:20:10 -0500

    initial commit

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myGames = new List<string>() {"Guilty Gear", "Super Smash Brothers", "Bloodborne", "Kirby's Superstar Saga", "Shin Megami Tensei 4" };
+
+            var orderGames = myGames.OrderBy(x => x.Length).ToList();
+            orderGames.ForEach(x => Console.WriteLine(x));
+        }
+    }
+}
